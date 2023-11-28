0f79848eec3eaa8955e8f9560792964fd9b09eb2
bigdiggysquiggle <randomcouchpotato@gmail.com>
2023-11-28 16:17:21 -0700

    updated to work with openai v1.3.5

--- ai_interface.py
+++ ai_interface.py
@@ -1,24 +1,22 @@
 #!/usr/bin/env python3
 
-import openai
+from openai import OpenAI
 import json
 import env
 
 class ai_interface():
-	_ai = openai
-	_system_prompts = []
-	_user_prompts = []
 
 	# Sets the internal fields to values passed in during class
-	# instantiation. Defaults to the values defined in ./env.py
-
+	# instantiation. Defaults to the values defined in env.py
 	# TODO?: class constructor that takes a file as argument,
 	# otherwise uses default values
 	def __init__(self, key=env.OPENAI_API_KEY, model=env.MODEL, max_token=env.TOKEN_MAX, ai_temp=env.TEMP):
-		self._ai.api_key = key
+		self._ai = OpenAI(api_key=key)
 		self.model = model
 		self.max_tokens = max_token
 		self.ai_temp = ai_temp
+		self.system_prompts = []
+		self.user_prompts = []
 
 	# Adds a string to the list of strings containing prompts
 	# which the model will not respond to. For example, you may
@@ -27,7 +25,7 @@ class ai_interface():
 	# it output a response
 	def add_system_prompt(self, prompt):
 		sys = {"role": "system", "content": prompt}
-		self._system_prompts.append(sys)
+		self.system_prompts.append(sys)
 
 	# Adds a string to the list of strings containing prompts
 	# that you do want it to respond to in the final response
@@ -36,14 +34,19 @@ class ai_interface():
 	# the model's responses to all the prompts in this list
 	def add_user_prompt(self, prompt):
 		use = {"role": "user", "content": prompt}
-		self._user_prompts.append(use)
+		self.user_prompts.append(use)
 
 	# Performs the actual api request and returns just the
 	# part of the response containing the text the model
 	# responded with
 	def interact(self):
 		messages = []
-		messages.extend(self._system_prompts)
-		messages.extend(self._user_prompts)
-		response = self._ai.ChatCompletion.create(model=self.model, max_tokens=self.max_tokens, temperature=self.ai_temp, messages=messages)
-		return response['choices'][0]['message']['content']
+		messages.extend(self.system_prompts)
+		messages.extend(self.user_prompts)
+		response = self._ai.chat.completions.create(
+				model=self.model,
+				messages=messages,
+				max_tokens=self.max_tokens,
+				temperature=self.ai_temp
+		)
+		return response.choices[0].message['content']
